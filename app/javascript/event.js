7eec5be642dd6d7ea31cdfc75aeaf602114282fc
Shota <1s2b1acksn0w@gmail.com>
2022-10-06 10:40:33 +0900

    変数名変更

--- app/javascript/event.js
+++ app/javascript/event.js
@@ -1,18 +1,18 @@
 window.addEventListener('load', function(){
 
-  const pullDownButton = document.getElementsByClassName("event_count")
-  const pullDownButtons = Array.from(pullDownButton)
+  const eventList = document.getElementsByClassName("event_count")
+  const eventLists = Array.from(eventList)
 
-  pullDownButtons.forEach(function(pullDownButton) {
-    pullDownButton.addEventListener('click', function(){
-      pullDownButton.firstElementChild.style.display = 'block';
+  eventLists.forEach(function(eventList) {
+    eventList.addEventListener('click', function(){
+      eventList.firstElementChild.style.display = 'block';
     })
 
     const closeButton = document.getElementsByClassName("close_btn")
     const closeButtons = Array.from(closeButton)
     closeButtons.forEach(function(closeButton) {
       closeButton.addEventListener('click', function(e) {
-        pullDownButton.firstElementChild.style.display = 'none';
+        eventList.firstElementChild.style.display = 'none';
         e.stopPropagation()
       })
     })
