34378c959a08b460f5a28a6814b2c75c6ec3d241
Shota <1s2b1acksn0w@gmail.com>
2022-10-06 14:26:11 +0900

    ユーザー機能追加

--- Gemfile
+++ Gemfile
@@ -56,3 +56,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'pry-rails'
 gem 'simple_calendar', '~> 2.0'
+gem 'devise'
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  belongs_to :user
+
   validates :title,      presence: true
   validates :content,    presence: true
   validates :start_time, presence: true
