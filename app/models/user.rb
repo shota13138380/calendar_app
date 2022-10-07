ab3bc25732711f099e76514dbd14bd6b0831a44d
Shota <1s2b1acksn0w@gmail.com>
2022-10-07 17:03:41 +0900

    rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :events,     dependent: :destroy
+  has_many :events, dependent: :destroy
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }, on: :create
